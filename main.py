ad98dc72a3c3d557da3500cf6fe6498bb3292efd
Garry Kuwanto <gkuwanto@gmail.com>
2021-11-02 14:39:47 +0700

    add line to plot, add loss to epoch plot

--- main.py
+++ main.py
@@ -9,13 +9,15 @@ np.random.seed(42)
 
 # Menambahkan argumen dalam cli
 parser = argparse.ArgumentParser()
-parser.add_argument("-obj", "--objective", help="Specify objective function of the optimization. Only MSE and MAE is available", default='MSE')
+parser.add_argument("-obj", "--objective",
+                    help="Specify objective function of the optimization. Only MSE and MAE is available", default='MSE')
 args = parser.parse_args()
 
 # Memastikan Nilai input argument
 if (args.objective) not in ["MSE", "MAE"]:
-    raise ValueError(f"Objective Function {args.objective} is not implemented yet")
-    
+    raise ValueError(
+        f"Objective Function {args.objective} is not implemented yet")
+
 # Membaca Data dari file
 data_train = read_csv('data/train.csv')
 data_test = read_csv('data/test.csv')
@@ -31,7 +33,6 @@ for col in headers:
         data_test[col] = [float(i) for i in data_test[col]]
 
 
-# TODO: One Hot Encode zipcode
 def onehotencode(data, col):
     key = np.unique(np.array(data[col]))
     inspect = data[col]
@@ -44,19 +45,20 @@ def onehotencode(data, col):
     del data[col]
     return {**data, **mydict}
 
+
 data_train = onehotencode(data_train, 'zipcode')
 data_test = onehotencode(data_test, 'zipcode')
 
 # Membaca Data menjadi format numpy
-X_train = np.array([data_train[col] for col in headers 
-    if col not in ['date', 'id', 'price', 'zipcode']]).T
+X_train = np.array([data_train[col] for col in headers
+                    if col not in ['date', 'id', 'price', 'zipcode']]).T
 y_train = np.array(data_train['price'])
 
-# Preprocessing X_train 
-X_mean = np.mean(X_train, axis = 0)
-X_std = np.std(X_train, axis = 0)
+# Preprocessing X_train
+X_mean = np.mean(X_train, axis=0)
+X_std = np.std(X_train, axis=0)
 X_train = (X_train - X_mean)/X_std
-X_train = np.concatenate([X_train, np.ones(X_train[:, 0:1].shape)], axis = 1)
+X_train = np.concatenate([X_train, np.ones(X_train[:, 0:1].shape)], axis=1)
 
 # Preprocessing y_train
 y_mean = np.mean(y_train)
@@ -65,20 +67,21 @@ y_train = (y_train - y_mean)/y_std
 
 
 # Inisialisasi Parameter Model
-theta = np.random.normal(size = X_train[0, :].shape)
+theta = np.random.normal(size=X_train[0, :].shape)
 
 # Penentuan Loss dan Gradient tergantung input
 if args.objective == 'MSE':
-    loss = lambda x, y, theta: 0.5 * (np.dot(theta, x) - y)**2
-    gradient = lambda x, y, theta: (np.dot(theta,x) - y) * x
+    def loss(x, y, theta): return 0.5 * (np.dot(theta, x) - y)**2
+    def gradient(x, y, theta): return (np.dot(theta, x) - y) * x
 elif args.objective == 'MAE':
-    loss = lambda x, y, theta: (np.abs(np.dot(theta, x)-y))
-    gradient = lambda x, y, theta: np.sign(np.dot(theta, x)-y) * x
+    def loss(x, y, theta): return (np.abs(np.dot(theta, x)-y))
+    def gradient(x, y, theta): return np.sign(np.dot(theta, x)-y) * x
 
 
 # Memulai Proses SGD
 n_epoch = 20
-eta = 0.0005
+eta = 0.00005
+loss_epoch = []
 for epoch in range(n_epoch):
     total_loss = 0
     total_sample = len(y_train)
@@ -87,13 +90,14 @@ for epoch in range(n_epoch):
         total_loss += loss(X_train[i], y_train[i], theta)
         grad = gradient(X_train[i], y_train[i], theta)
         theta = theta - (eta*grad)
+    loss_epoch.append(total_loss/total_sample)
     print(f"Epoch: {epoch} \t Loss: {total_loss/total_sample}")
 
 # Melakukan Preprocessing pada Dataset Test
-X_test = np.array([data_test[col] for col in headers 
-    if col not in ['date', 'id', 'price', 'zipcode']]).T
+X_test = np.array([data_test[col] for col in headers
+                   if col not in ['date', 'id', 'price', 'zipcode']]).T
 X_test = (X_test - X_mean)/X_std
-X_test = np.concatenate([X_test, np.ones(X_test[:, 0:1].shape)], axis = 1)
+X_test = np.concatenate([X_test, np.ones(X_test[:, 0:1].shape)], axis=1)
 
 # Menghitung Prediksi
 y_pred = np.dot(theta, X_test.T)
@@ -106,16 +110,40 @@ with open('data/prediction.csv', 'w+') as f:
     for id, price in zip(data_test['id'], y_pred):
         f.write(f'{id},{price}\n')
 
-X_headers = [col for col in headers 
-    if col not in ['date', 'id', 'price', 'zipcode']]
+# TODO: Evaluate Result
+# Read Prediction
+# Read True Label
+# Print MSE / MAE
+
+
+X_headers = [col for col in headers
+             if col not in ['date', 'id', 'price', 'zipcode']]
 
 y = y_train
 for i in range(len(X_headers)):
     x = []
+    x_mean = []
     for j in range(len(X_train)):
+        input_mean = np.mean(X_train, axis=0)
+        input_mean[i] = X_train[j][i]
+
         x.append(X_train[j][i])
+        x_mean.append(input_mean)
+    y_pred_mean = np.dot(theta, np.array(x_mean).T)
+    minimum = min(y_pred_mean)
+    maximum = max(y_pred_mean)
+
     plt.figure()
     plt.scatter(x, y)
+    plt.ylim([minimum-1, maximum+5])
+    plt.plot(x, y_pred_mean, color='r', linewidth=1.5)
     plt.xlabel(X_headers[i])
     plt.ylabel('price')
     plt.savefig(X_headers[i] + ' to price.png')
+
+plt.figure()
+plt.scatter(range(len(loss_epoch)), loss_epoch)
+plt.xticks(range(len(loss_epoch)))
+plt.xlabel('Epoch')
+plt.ylabel('Loss')
+plt.savefig('loss to epoch.png')
